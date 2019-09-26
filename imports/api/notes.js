c387953398253e4678c36def6d819961d82b074a
JacoDTDev <jacodtdev@gmail.com>
2019-09-26 15:41:17 +0200

    120. Testing Note Remove

--- imports/api/notes.js
+++ imports/api/notes.js
@@ -1,6 +1,7 @@
 import { Mongo } from 'meteor/mongo';
 import { Meteor } from 'meteor/meteor';
 import moment from 'moment';
+import SimpleSchema from 'simpl-schema'
 
 export const Notes = new Mongo.Collection('notes');
 
@@ -16,5 +17,21 @@ Meteor.methods({
       userId: this.userId,
       updatedAt: moment().valueOf()
     });
+  },
+  'notes.remove'(_id){
+    //check for userId,else throw Error
+    if (!this.userId) {
+      throw new Meteor.Error('not-authorized');
+    }
+    //SimpleSchme to validate _id string with length greater than 1
+    new SimpleSchema({
+     _id:{
+        type: String,
+        min: 1
+     }
+    }).validate({_id});
+    //Notes.remove to remove the notes
+    Notes.remove({_id, userId:this.userId});
+
   }
 });
--- imports/api/notes.test.js
+++ imports/api/notes.test.js
@@ -5,7 +5,19 @@ import { Notes } from './notes';
 
 if (Meteor.isServer) {
   describe('notes', function () {
-
+    //seed data
+    //test data
+    beforeEach(function () {
+      Notes.remove({}); //this do not effect our normal data
+      Notes.insert({
+        _id: 'testNoteId1',
+        title:'test title',
+        body:'My body for note',
+        updatedAt:0,
+        userId:'testUserId'
+      });
+    });
+    //insert note test
     it('should insert new note', function () {
       const userId = 'testid';
       const _id = Meteor.server.method_handlers['notes.insert'].apply({ userId });
@@ -18,6 +30,23 @@ if (Meteor.isServer) {
         Meteor.server.method_handlers['notes.insert']();
       }).toThrow();
     });
+    //remove note test
+    it('should remove note if authenticated', function () {
+      Meteor.server.method_handlers['notes.remove'].apply({userId: 'testUserId'},['testNoteId1']);
 
+      expect(Notes.findOne({_id:'testNoteId1'})).toNotExist();
+    });
+    
+    it('should not remove note if unauthenticated',function () {
+      expect(()=>{
+        Meteor.server.method_handlers['notes.remove'].apply({},['testNoteId1']);
+      }).toThrow();
+    });
+
+    it('should not remove note if invalid_id',function () {
+      expect(()=>{
+        Meteor.server.method_handlers['notes.remove'].apply({userId: 'testUserId'});
+      }).toThrow();
+    });
   });
 }
