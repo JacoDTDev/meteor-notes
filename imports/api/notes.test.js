97ecd0a7b46dda58c3b767f4acc684b455b04975
JacoDTDev <jacodtdev@gmail.com>
2019-09-27 09:13:16 +0200

    121.2 Created const that set equal to the test data

--- imports/api/notes.test.js
+++ imports/api/notes.test.js
@@ -5,17 +5,18 @@ import { Notes } from './notes';
 
 if (Meteor.isServer) {
   describe('notes', function () {
+    const noteOne = {
+      _id: 'testNoteId1',
+      title:'test title',
+      body:'My body for note',
+      updatedAt:0,
+      userId:'testUserId'
+    };
     //seed data
     //test data
     beforeEach(function () {
       Notes.remove({}); //this do not effect our normal data
-      Notes.insert({
-        _id: 'testNoteId1',
-        title:'test title',
-        body:'My body for note',
-        updatedAt:0,
-        userId:'testUserId'
-      });
+      Notes.insert(noteOne);
     });
     //insert note test
     it('should insert new note', function () {
@@ -32,20 +33,20 @@ if (Meteor.isServer) {
     });
     //remove note test
     it('should remove note if authenticated', function () {
-      Meteor.server.method_handlers['notes.remove'].apply({userId: 'testUserId'},['testNoteId1']);
+      Meteor.server.method_handlers['notes.remove'].apply({userId: noteOne.userId},[noteOne._id]);
 
-      expect(Notes.findOne({_id:'testNoteId1'})).toNotExist();
+      expect(Notes.findOne({_id:noteOne._id})).toNotExist();
     });
     
     it('should not remove note if unauthenticated',function () {
       expect(()=>{
-        Meteor.server.method_handlers['notes.remove'].apply({},['testNoteId1']);
+        Meteor.server.method_handlers['notes.remove'].apply({},[noteOne._id]);
       }).toThrow();
     });
 
     it('should not remove note if invalid_id',function () {
       expect(()=>{
-        Meteor.server.method_handlers['notes.remove'].apply({userId: 'testUserId'});
+        Meteor.server.method_handlers['notes.remove'].apply({userId: noteOne.userId});
       }).toThrow();
     });
     it('should update note',function () {
