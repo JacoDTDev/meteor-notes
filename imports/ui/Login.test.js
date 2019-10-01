b02b61c6091d35cf3c3317c61673cd31887dda45
JacoDTDev <jacodtdev@gmail.com>
2019-10-01 15:08:02 +0200

    129. Testing Login - Setting Input and Submitting

--- imports/ui/Login.test.js
+++ imports/ui/Login.test.js
@@ -20,5 +20,29 @@ if (Meteor.isClient){
             expect(wrapper.find('p').length).toBe(0);
         });
 
+        it('should call loginWithPassword with the form data',function () {
+            const email = 'jaco@test.com';
+            const password = 'password123';
+            const spy = expect.createSpy();
+            const wrapper = mount(<Login loginWithPassword={spy}/>);
+
+            wrapper.ref('email').node.value = email;
+            wrapper.ref('password').node.value = password;
+            wrapper.find('form').simulate('submit');
+
+            expect(spy.calls[0].arguments[0]).toEqual({email});
+            expect(spy.calls[0].arguments[1]).toBe(password);
+        });
+        
+        it('should sat loginWithPassword callback errors',function () {
+            const spy = expect.createspy();
+            const wrapper = mount(<Login loginWithPassword={spy}/>);
+
+            wrapper.find('form').sibling('submit');
+            spy.calls[0].arguments[2]({});
+            expect(wrapper.state('error').length).toNotBe(0);
+            spy.calls[0].arguments[2]({});
+            expect(wrapper.state('error').length).toNBe(0);
+        });
     });
 }
