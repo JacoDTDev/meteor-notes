d3be7acdc3d26dff31e0a30fd24f6038944f9138
JacoDTDev <jacodtdev@gmail.com>
2019-10-04 10:04:29 +0200

    137. Test Fixtures

--- imports/ui/NoteListItem.test.js
+++ imports/ui/NoteListItem.test.js
@@ -1,27 +1,40 @@
 import React from 'react';
 import expect from 'expect';
 import {mount} from 'enzyme';
-import{meteor} from 'meteor/meteor';
+import{Meteor} from 'meteor/meteor';
 
-import NoteListItem from "./NoteListItem";
+import {notes} from '../fixtures/fixtures'
+import {NoteListItem} from "./NoteListItem";
 
 if (Meteor.isClient){
     describe('NoteListItem', function () {
-        it('should render title and timestamp',function () {
-            const title = 'My title here';
-            const updateAt = 1570091009324;
-            const wrapper = mount(<NoteListItem note={{title,updateAt}}/>);
+        let Session;
 
-            expect(wrapper.find('h5').text()).toBe(title);
-            expect(wrapper.find('p').text()).toBe('10/03/19');
+        beforeEach(()=>{
+            Session ={
+                set: expect.createSpy()
+            }
+        });
+
+        it('should render title and timestamp',function () {
+            const wrapper = mount(<NoteListItem note={notes[0]} Session={Session}/>);
+
+            expect(wrapper.find('h5').text()).toBe(notes[0].title);
+            expect(wrapper.find('p').text()).toBe('10/04/19');
         });
         it('should set default title if no title set',function () {
-            const title = '';
-            const updateAt = 1570091009324;
-            const wrapper = mount(<NoteListItem note={{title,updateAt}}/>);
+            const wrapper = mount(<NoteListItem note={notes[1]} Session={Session}/>);
 
             expect(wrapper.find('h5').text()).toBe('Untitled note');
 
-        })
+        });
+        it('should call set on click',function () {
+            //Render NoteListItem using either note and Session
+            const wrapper = mount(<NoteListItem note={notes[1]} Session={Session}/>);
+            //Find div and simulate click event
+            wrapper.find('div').simulate('click');
+            //Expect Session.set to have been called with some arguments
+            expect(Session.set).toHaveBeenCalledWith('selectedNoteId', notes[1]._id);
+        });
     });
 }
