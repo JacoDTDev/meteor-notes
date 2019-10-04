0e53dec16d3248b50a86b53dbdc63d53e3e43479
JacoDTDev <jacodtdev@gmail.com>
2019-10-04 11:55:43 +0200

    139. The Editor Component part1

---
+++ imports/ui/Editer.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {createContainer} from 'meteor/react-meteor-data';
+import {Session} from 'meteor/session';
+
+import {Notes} from "../api/notes";
+
+export class Editor extends React.Component{
+    render(){
+        //We get a note
+        //We get an id, but it`s not a match
+        //WE get nothing
+        if(this.props.note){
+            return(
+                <p>We got the note!</p>
+            );
+        }else if (this.props.selectedNoteId){
+            return(
+                <p>Note not found</p>
+            );
+        }else{
+            return(
+                <p>Pick or create a note to get started.</p>
+            );
+        }
+    }
+}
+
+Editor.propTypes ={
+    notes: React.PropTypes.object,
+    selectedNoteId: React.PropTypes.string
+};
+
+export default createContainer(()=>{
+    const selectedNoteId = Session.get('selectedNoteId');
+
+    return{
+        selectedNoteId,
+        note: Notes.findOne(selectedNoteId)
+    };
+}, Editor);
