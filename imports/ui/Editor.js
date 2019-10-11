0a2c90edb63f8fef60873bab2582c03f7d561997
JacoDTDev <jacodtdev@gmail.com>
2019-10-11 16:58:48 +0200

    150. Creating the Side-By-Side Layout

--- imports/ui/Editor.js
+++ imports/ui/Editor.js
@@ -2,42 +2,42 @@ import React from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
 import { Session } from 'meteor/session';
 import { Meteor } from 'meteor/meteor';
-import {browserHistory} from "react-router";
+import { browserHistory } from 'react-router';
 
 import { Notes } from '../api/notes';
 
 export class Editor extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state ={
-      title:'',
-      body:''
+    this.state = {
+      title: '',
+      body: ''
     };
   }
   handleBodyChange(e) {
-    const body =e.target.value;
-    this.setState({body});
-    this.props.call('notes.update', this.props.note._id, {body});
+    const body = e.target.value;
+    this.setState({ body });
+    this.props.call('notes.update', this.props.note._id, { body });
   }
   handleTitleChange(e) {
-    const title =e.target.value;
-    this.setState({title});
-    this.props.call('notes.update', this.props.note._id, {title});
+    const title = e.target.value;
+    this.setState({ title });
+    this.props.call('notes.update', this.props.note._id, { title });
   }
-  componentDidUpdate(prevProps, prevState){
-      const currentNoteId = this.props.note?this.props.note._id:undefined;
-      const prevNoteId = prevProps.note?prevProps.note._id:undefined;
+  handleRemoval(){
+    this.props.call('notes.remove', this.props.note._id);
+    this.props.browserHistory.push('/dashboard');
+  }
+  componentDidUpdate(prevProps, prevState) {
+    const currentNoteId = this.props.note ? this.props.note._id : undefined;
+    const prevNoteId = prevProps.note ? prevProps.note._id : undefined;
 
-      if(currentNoteId && currentNoteId !==prevNoteId){
-          this.setState({
-              title:this.props.note.title,
-              body:this.props.note.body
-          });
-      }
-  }
-  deleteNote(){
-      this.props.call('notes.remove', this.props.note._id);
-      this.props.browserHistory.push('/dashboard');
+    if (currentNoteId && currentNoteId !== prevNoteId) {
+      this.setState({
+        title: this.props.note.title,
+        body: this.props.note.body
+      });
+    }
   }
   render() {
     if (this.props.note) {
@@ -45,7 +45,7 @@ export class Editor extends React.Component {
         <div>
           <input value={this.state.title} placeholder="Untitled Note" onChange={this.handleTitleChange.bind(this)}/>
           <textarea value={this.state.body} placeholder="Your note here" onChange={this.handleBodyChange.bind(this)}></textarea>
-          <button onClick={this.deleteNote.bind(this)}>Delete Note</button>
+          <button onClick={this.handleRemoval.bind(this)}>Delete Note</button>
         </div>
       );
     } else {
--- imports/ui/NoteListHeader.test.js
+++ imports/ui/NoteListHeader.test.js
@@ -4,7 +4,7 @@ import { mount } from 'enzyme';
 import { Meteor } from 'meteor/meteor';
 
 import { NoteListHeader } from './NoteListHeader';
-import {notes} from '../fixtures/fixtures';
+import { notes } from '../fixtures/fixtures';
 
 if (Meteor.isClient) {
   describe('NoteListHeader', function () {
@@ -13,22 +13,23 @@ if (Meteor.isClient) {
 
     beforeEach(function () {
       meteorCall = expect.createSpy();
-      Session ={
-        set:expect.createSpy()
+      Session = {
+        set: expect.createSpy()
       }
     });
 
     it('should call meteorCall on click', function () {
-      const wrapper = mount(<NoteListHeader meteorCall={meteorCall} Session ={Session}/>);
+      const wrapper = mount(<NoteListHeader meteorCall={meteorCall} Session={Session}/>);
 
       wrapper.find('button').simulate('click');
       meteorCall.calls[0].arguments[1](undefined, notes[0]._id);
 
       expect(meteorCall.calls[0].arguments[0]).toBe('notes.insert');
-      expect(Session.set).toHaveBeenCalledWith('selectedNoteId',notes[0]._id);
+      expect(Session.set).toHaveBeenCalledWith('selectedNoteId', notes[0]._id);
     });
+
     it('should not set session for failed insert', function () {
-      const wrapper = mount(<NoteListHeader meteorCall={meteorCall} Session ={Session}/>);
+      const wrapper = mount(<NoteListHeader meteorCall={meteorCall} Session={Session}/>);
 
       wrapper.find('button').simulate('click');
       meteorCall.calls[0].arguments[1]({}, undefined);
