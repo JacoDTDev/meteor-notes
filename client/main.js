300f454617a125f1813d34d50cca9ffc1e79e0d8
JacoDTDev <jacodtdev@gmail.com>
2019-10-15 12:52:05 +0200

    151 Styling Sidebar and Main Container

--- client/main.js
+++ client/main.js
@@ -2,25 +2,27 @@ import { Meteor } from 'meteor/meteor';
 import ReactDOM from 'react-dom';
 import { Tracker } from 'meteor/tracker';
 import { Session } from 'meteor/session';
-import {browserHistory} from "react-router";
+import { browserHistory } from 'react-router';
 
 import { routes, onAuthChange } from '../imports/routes/routes';
 import '../imports/startup/simple-schema-configuration.js';
 
 Tracker.autorun(() => {
   const isAuthenticated = !!Meteor.userId();
-  const currentPagePrivacy=Session.get('currentPagePrivacy');
+  const currentPagePrivacy = Session.get('currentPagePrivacy');
 
   onAuthChange(isAuthenticated, currentPagePrivacy);
 });
-Tracker.autorun(()=>{
+
+Tracker.autorun(() => {
   const selectedNoteId = Session.get('selectedNoteId');
 
-  if(selectedNoteId){
-   browserHistory.replace(`/dashboard/${selectedNoteId}`);
+  if (selectedNoteId) {
+    browserHistory.replace(`/dashboard/${selectedNoteId}`);
   }
 });
+
 Meteor.startup(() => {
-  Session.set('selectedNoteId',undefined);
+  Session.set('selectedNoteId', undefined);
   ReactDOM.render(routes, document.getElementById('app'));
 });
